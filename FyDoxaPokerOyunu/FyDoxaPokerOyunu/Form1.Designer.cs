f85bcc9e90e3f11c6093da4890379e5d312833db
Fatih YILDIZHAN <fatih@doxalabs.co.uk>
2020-03-11 21:51:15 +0300

    add poker

---
+++ FyDoxaPokerOyunu/FyDoxaPokerOyunu/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace FyDoxaPokerOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lstBoxOync3 = new System.Windows.Forms.ListBox();
+            this.lstBoxOync4 = new System.Windows.Forms.ListBox();
+            this.lstBoxOync2 = new System.Windows.Forms.ListBox();
+            this.lstBoxOync1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDagit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.DarkGreen;
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Controls.Add(this.panel3);
+            this.groupBox1.Controls.Add(this.panel4);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lstBoxOync3);
+            this.groupBox1.Controls.Add(this.lstBoxOync4);
+            this.groupBox1.Controls.Add(this.lstBoxOync2);
+            this.groupBox1.Controls.Add(this.lstBoxOync1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnDagit);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(916, 513);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Poker";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(35, 199);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(185, 133);
+            this.panel1.TabIndex = 15;
+            this.panel1.MouseHover += new System.EventHandler(this.panel1_MouseHover);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(368, 353);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(185, 133);
+            this.panel2.TabIndex = 14;
+            this.panel2.MouseHover += new System.EventHandler(this.panel2_MouseHover);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(695, 196);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(185, 133);
+            this.panel3.TabIndex = 14;
+            this.panel3.MouseHover += new System.EventHandler(this.panel3_MouseHover);
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(368, 37);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(185, 133);
+            this.panel4.TabIndex = 14;
+            this.panel4.MouseHover += new System.EventHandler(this.panel4_MouseHover);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(691, 363);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 21);
+            this.label8.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(561, 428);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 21);
+            this.label7.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(561, 79);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 21);
+            this.label6.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 367);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 21);
+            this.label5.TabIndex = 9;
+            // 
+            // lstBoxOync3
+            // 
+            this.lstBoxOync3.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.lstBoxOync3.ColumnWidth = 2;
+            this.lstBoxOync3.FormattingEnabled = true;
+            this.lstBoxOync3.ItemHeight = 21;
+            this.lstBoxOync3.Location = new System.Drawing.Point(695, 196);
+            this.lstBoxOync3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstBoxOync3.Name = "lstBoxOync3";
+            this.lstBoxOync3.Size = new System.Drawing.Size(184, 130);
+            this.lstBoxOync3.TabIndex = 8;
+            this.lstBoxOync3.MouseLeave += new System.EventHandler(this.lstBoxOync3_MouseLeave);
+            this.lstBoxOync3.MouseHover += new System.EventHandler(this.lstBoxOync3_MouseHover);
+            // 
+            // lstBoxOync4
+            // 
+            this.lstBoxOync4.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.lstBoxOync4.ColumnWidth = 2;
+            this.lstBoxOync4.FormattingEnabled = true;
+            this.lstBoxOync4.ItemHeight = 21;
+            this.lstBoxOync4.Location = new System.Drawing.Point(368, 39);
+            this.lstBoxOync4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstBoxOync4.Name = "lstBoxOync4";
+            this.lstBoxOync4.Size = new System.Drawing.Size(184, 130);
+            this.lstBoxOync4.TabIndex = 7;
+            this.lstBoxOync4.MouseLeave += new System.EventHandler(this.lstBoxOync4_MouseLeave);
+            this.lstBoxOync4.MouseHover += new System.EventHandler(this.lstBoxOync4_MouseHover);
+            // 
+            // lstBoxOync2
+            // 
+            this.lstBoxOync2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.lstBoxOync2.ColumnWidth = 2;
+            this.lstBoxOync2.FormattingEnabled = true;
+            this.lstBoxOync2.ItemHeight = 21;
+            this.lstBoxOync2.Location = new System.Drawing.Point(368, 356);
+            this.lstBoxOync2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstBoxOync2.Name = "lstBoxOync2";
+            this.lstBoxOync2.Size = new System.Drawing.Size(184, 130);
+            this.lstBoxOync2.TabIndex = 6;
+            this.lstBoxOync2.MouseLeave += new System.EventHandler(this.lstBoxOync2_MouseLeave);
+            this.lstBoxOync2.MouseHover += new System.EventHandler(this.lstBoxOync2_MouseHover);
+            // 
+            // lstBoxOync1
+            // 
+            this.lstBoxOync1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.lstBoxOync1.ColumnWidth = 2;
+            this.lstBoxOync1.FormattingEnabled = true;
+            this.lstBoxOync1.ItemHeight = 21;
+            this.lstBoxOync1.Location = new System.Drawing.Point(35, 199);
+            this.lstBoxOync1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstBoxOync1.Name = "lstBoxOync1";
+            this.lstBoxOync1.Size = new System.Drawing.Size(184, 130);
+            this.lstBoxOync1.TabIndex = 5;
+            this.lstBoxOync1.MouseLeave += new System.EventHandler(this.lstBoxOync1_MouseLeave);
+            this.lstBoxOync1.MouseHover += new System.EventHandler(this.lstBoxOync1_MouseHover);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(273, 37);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 21);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Oyuncu 4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(691, 332);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Oyuncu 3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(561, 465);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Oyuncu 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 336);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Oyuncu 1";
+            // 
+            // btnDagit
+            // 
+            this.btnDagit.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnDagit.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDagit.ForeColor = System.Drawing.Color.Maroon;
+            this.btnDagit.Location = new System.Drawing.Point(368, 196);
+            this.btnDagit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDagit.Name = "btnDagit";
+            this.btnDagit.Size = new System.Drawing.Size(185, 134);
+            this.btnDagit.TabIndex = 0;
+            this.btnDagit.Text = "Dağıt";
+            this.btnDagit.UseVisualStyleBackColor = false;
+            this.btnDagit.Click += new System.EventHandler(this.btnDagit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(948, 544);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Poker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox lstBoxOync3;
+        private System.Windows.Forms.ListBox lstBoxOync4;
+        private System.Windows.Forms.ListBox lstBoxOync2;
+        private System.Windows.Forms.ListBox lstBoxOync1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDagit;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ FyDoxaPokerOyunu/FyDoxaPokerOyunu/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FyDoxaPokerOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Metod Tanımları
+        string KartSorgusu(string[] ara, string[] kartlar)
+        {
+            int limit = 0;
+            string str;
+            for (int i = 0; i < kartlar.Length; i++)
+            {
+                int sayac = 0;
+                // oyuncunun elinde en fazla kaç tane kartın aynı olduğu bulunur
+                for (int j = 0; j < ara.Length; j++)
+                {
+                    if (kartlar[i] == ara[j])
+                    {
+                        sayac++;
+                    }
+                }
+                if (sayac > limit)
+                {
+                    limit = sayac;
+                }
+            }
+            // kullanıcının elindeki aynı kart sayısına göre koşullar çalışır..
+            if (limit == 5)
+            {
+                str = "Elinizde bir beşli var";
+            }
+            else if (limit == 4)
+            {
+                str = "Elinizde bir dörtlü var";
+            }
+            else if (limit == 3)
+            {
+                str = "Elinizde üçlü var";
+            }
+            else if (limit == 2)
+            {
+                str = "Elinizde ikili var";
+            }
+            else
+            {
+                str = "Elinizde bişey yok";
+            }
+            return str; // çalışan koşula göre geriye str nin değeri döndürülür...
+        }
+        //Dizi Tanımları
+        string[] kartTur = new string[] { "Maça", "Karo", "Sinek", "Kupa" };
+        string[] kartNo = new string[] { "As", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Vale", "Kız", "Papaz" };
+        string[] kartlar = new string[52]; // tüm kartların tutulacağı dizi
+        int[] sayi20 = new int[0]; // oyunculara rastgele ve farklı kart dağıtılmada kullanılan dizi
+        //her bir oyuncuya dağılan kartlar için kullanılan diziler
+        string[] oync1dizi = new string[11];
+        string[] oync2dizi = new string[11];
+        string[] oync3dizi = new string[11];
+        string[] oync4dizi = new string[11];
+        //Sayı Üretme
+        Random r = new Random();
+        //her bir oyuncuya gelen kartların birleşik hali
+        string oyuncu1Kartlar;
+        string oyuncu2Kartlar;
+        string oyuncu3Kartlar;
+        string oyuncu4Kartlar;
+
+        //Dağıt Butonu
+        private void btnDagit_Click(object sender, EventArgs e)
+        {
+            // her oyun öncesi Biras temislik Şart !
+            lstBoxOync1.Items.Clear();
+            oyuncu1Kartlar = "";
+            lstBoxOync2.Items.Clear();
+            oyuncu2Kartlar = "";
+            lstBoxOync3.Items.Clear();
+            oyuncu3Kartlar = "";
+            lstBoxOync4.Items.Clear();
+            oyuncu4Kartlar = "";
+            //Oyunculara rastgele ve farklı kartlar dağıtmak için 20 sayı üretme
+            for (int i = 0; i < 20; )
+            {
+                int randomNumbers = r.Next(0, kartlar.Length);
+                Array.Resize(ref sayi20, i + 1);
+                if (Array.IndexOf(sayi20, randomNumbers) == -1)
+                {
+                    sayi20[i] = randomNumbers;
+                    i++;
+                }
+            }
+            // oyunculara kartlarını dağıtma zamanı
+            // üretilen 20 sayı 5  erli olarak kartlar dizisinden rastgele alınarak herbir oyuncuya dağıtılır
+            // aynı zamanda daha sonra kullanılmak üzere oyuncuKartlar Dizilerine birleştirilerek atılır
+            for (int i = 0; i < 5; i++)
+            {
+                lstBoxOync1.Items.Add(kartlar[sayi20[i]]);
+                lstBoxOync2.Items.Add(kartlar[sayi20[i + 5]]);
+                lstBoxOync3.Items.Add(kartlar[sayi20[i + 10]]);
+                lstBoxOync4.Items.Add(kartlar[sayi20[i + 15]]);
+
+                oyuncu1Kartlar += kartlar[sayi20[i]] + "-";
+                oyuncu2Kartlar += kartlar[sayi20[i + 5]] + "-";
+                oyuncu3Kartlar += kartlar[sayi20[i + 10]] + "-";
+                oyuncu4Kartlar += kartlar[sayi20[i + 15]] + "-";
+            }
+            // her bir oyuncunun elinde neler olduğunu oyuncuya göstermek için kart bilgileri ayrılarak dizilerde tutulur...
+            oync1dizi = oyuncu1Kartlar.Split('-');
+            oync2dizi = oyuncu2Kartlar.Split('-');
+            oync3dizi = oyuncu3Kartlar.Split('-');
+            oync4dizi = oyuncu4Kartlar.Split('-');
+        }
+        // Formun load esnasında kartlar oluşturulur..
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < kartTur.Length; i++)
+            {
+                for (int x = 0; x < kartNo.Length; x++)
+                {
+                    kartlar[i * 13 + x] = kartTur[i] + "-" + kartNo[x];
+                }
+            }
+        }
+        // Listbox ların MouseHover özelliği sayesinde kullanıcıya elinde kartların bilgisi KartSorgusu metodu ile gösterilir
+        // Mouse leave olduğunda oyuncuların altında poker 2008 yazdırılır...
+
+        private void lstBoxOync1_MouseHover(object sender, EventArgs e)
+        {
+            label5.Text = KartSorgusu(oync1dizi, kartNo);
+        }
+
+        private void lstBoxOync1_MouseLeave(object sender, EventArgs e)
+        {
+            label5.Text = "FyDxPoker2008";
+            panel1.Visible = true;
+         }
+
+        private void lstBoxOync2_MouseHover(object sender, EventArgs e)
+        {
+            label7.Text = KartSorgusu(oync2dizi, kartNo);
+        }
+
+        private void lstBoxOync2_MouseLeave(object sender, EventArgs e)
+        {
+            label7.Text = "FyDxPoker2008";
+            panel2.Visible = true;
+        }
+
+        private void lstBoxOync3_MouseHover(object sender, EventArgs e)
+        {
+            label8.Text = KartSorgusu(oync3dizi, kartNo);
+        }
+
+        private void lstBoxOync3_MouseLeave(object sender, EventArgs e)
+        {
+            label8.Text = "FyDxPoker2008";
+            panel3.Visible = true;
+        }
+
+        private void lstBoxOync4_MouseHover(object sender, EventArgs e)
+        {
+            label6.Text = KartSorgusu(oync4dizi, kartNo);
+        }
+
+        private void lstBoxOync4_MouseLeave(object sender, EventArgs e)
+        {
+            label6.Text = "FyDxPoker2008";
+            panel4.Visible = true;
+        }
+
+        private void panel4_MouseHover(object sender, EventArgs e)
+        {
+            panel4.Visible = false;
+        }
+
+        private void panel1_MouseHover(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void panel3_MouseHover(object sender, EventArgs e)
+        {
+            panel3.Visible = false;
+        }
+
+        private void panel2_MouseHover(object sender, EventArgs e)
+        {
+            panel2.Visible = false;
+        }   
+    }
+    //Fatih Yıldızhan && DoXa && 09'
+    //fatih@yildizhanlar.com
+    //www.fatihyildizhan.com
+}
