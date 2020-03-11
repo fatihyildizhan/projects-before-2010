c397b52d4c7b0941abc66e2fbdb4ffa68defdd20
Fatih YILDIZHAN <fatih@doxalabs.co.uk>
2020-03-11 21:55:56 +0300

    add ünlü uyumu

---
+++ FyDoxaUnluUyumu/FyDoxaUnluUyumu/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace FyDoxaUnluUyumu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pnlDurum = new System.Windows.Forms.Panel();
+            this.lblMesaj = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnKontrol = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.pnlDurum.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pnlDurum);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.btnKontrol);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(339, 197);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ünlü Uyumu";
+            // 
+            // pnlDurum
+            // 
+            this.pnlDurum.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.pnlDurum.Controls.Add(this.lblMesaj);
+            this.pnlDurum.Location = new System.Drawing.Point(8, 119);
+            this.pnlDurum.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlDurum.Name = "pnlDurum";
+            this.pnlDurum.Size = new System.Drawing.Size(313, 43);
+            this.pnlDurum.TabIndex = 14;
+            this.pnlDurum.Visible = false;
+            // 
+            // lblMesaj
+            // 
+            this.lblMesaj.AutoSize = true;
+            this.lblMesaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMesaj.ForeColor = System.Drawing.Color.Transparent;
+            this.lblMesaj.Location = new System.Drawing.Point(4, 10);
+            this.lblMesaj.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMesaj.Name = "lblMesaj";
+            this.lblMesaj.Size = new System.Drawing.Size(131, 25);
+            this.lblMesaj.TabIndex = 2;
+            this.lblMesaj.Text = "Kelime Giriniz";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(8, 79);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(312, 32);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.Text = "Kelime Giriniz";
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            // 
+            // btnKontrol
+            // 
+            this.btnKontrol.Enabled = false;
+            this.btnKontrol.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKontrol.Location = new System.Drawing.Point(8, 23);
+            this.btnKontrol.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnKontrol.Name = "btnKontrol";
+            this.btnKontrol.Size = new System.Drawing.Size(313, 37);
+            this.btnKontrol.TabIndex = 12;
+            this.btnKontrol.Text = "Kontrol Et";
+            this.btnKontrol.UseVisualStyleBackColor = true;
+            this.btnKontrol.Click += new System.EventHandler(this.btnKontrol_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(371, 233);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ünlü Uyumu";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.pnlDurum.ResumeLayout(false);
+            this.pnlDurum.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel pnlDurum;
+        private System.Windows.Forms.Label lblMesaj;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnKontrol;
+    }
+}
+
---
+++ FyDoxaUnluUyumu/FyDoxaUnluUyumu/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FyDoxaUnluUyumu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Dizi Tanımları
+        char[] inceSesli = new char[] { 'e', 'i', 'ü', 'ö' };
+        char[] kalinSesli = new char[] { 'a', 'ı', 'u', 'o' };
+        char[] sesliHarfler = new char[] { 'e', 'i', 'ü', 'ö', 'a', 'ı', 'u', 'o' };
+        // Method Tanımları
+        //sesKontrol Metodu textbox a girilen kelimedeki sesli harfleri alır , Boşluk varsa Uyarı verir
+        //panel metodu aldığı değere göre panelin rengini değiştirir
+        char[] sesKontrol(string yazi, char[] harfDizisi)
+        {
+            int sayac = 0;
+            char[] harfTipi = new char[0];
+
+            for (int i = 0; i < yazi.Length; i++)
+            {
+                if (yazi[i].ToString() == " ")
+                {
+                    MessageBox.Show("Lütfen Boşluk Bırakmayınız");
+                    aradaBoslukVarmi = true;
+                    break;
+                }
+                else
+                {
+                    for (int j = 0; j < harfDizisi.Length; j++)
+                    {
+                        if (harfDizisi[j] == yazi[i])
+                        {
+                            Array.Resize(ref harfTipi, sayac + 1);
+                            harfTipi[sayac] = harfDizisi[j];
+                            sayac++;
+                        }
+                        else { }
+                    }
+                }
+            }
+            return harfTipi;
+        }
+        void panel(bool kontrol)
+        {
+            pnlDurum.Visible = true;
+            if (kontrol == true)
+            {
+                pnlDurum.BackColor = Color.Lime;
+            }
+            else
+            {
+                pnlDurum.BackColor = Color.Red;
+            }
+        }
+        bool aradaBoslukVarmi = false;
+        //kontrol et butonu
+        //Programın beyni beyni :D
+        private void btnKontrol_Click(object sender, EventArgs e)
+        {
+            btnKontrol.Enabled = false;
+            if (textBox1.Text != "")
+            {
+                char[] harfler = sesKontrol(textBox1.Text.ToLower(), sesliHarfler);
+                if (aradaBoslukVarmi == false)
+                {
+                    bool sesKalinMi = true;
+                    bool sesVarMi = true;
+                    int sayacHarf = harfler.Length;
+                    for (int i = 0; i < 3; i++)  // ilk harfin ince sesli mi kalin sesli mi olduğu belirlenir
+                    {
+                        if (sayacHarf == 0)
+                        {
+                            MessageBox.Show("insan ol sesli harf gir");
+                            sesVarMi = false;
+                            break;
+                        }
+                        else if (harfler[0] == inceSesli[i])
+                        {
+                            sesKalinMi = false;
+                            break;
+                        }
+                        else { }
+                    }
+                    if (sesKalinMi == true && sesVarMi == true) // ilk sesli harf kalın ise bu koşul çalışır
+                    {
+                        bool digerSslrKlnMi = true;
+                        for (int i = 0; i < harfler.Length; i++)
+                        {
+                            int sesKontrol1 = Array.IndexOf(kalinSesli, harfler[i]); //Diğer sesli harfler kalın mı ?
+                            if (sesKontrol1 == -1)
+                            {
+                                digerSslrKlnMi = false;
+                            }
+                        }
+                        if (digerSslrKlnMi == true)
+                        {
+                            panel(true);
+                            lblMesaj.Text = "B.Ü.U 'na uygundur.";
+                        }
+                        else
+                        {
+                            panel(false);
+                            lblMesaj.Text = "B.Ü.U'na uygun değildir.";
+                        }
+                    }
+                    else if (sesKalinMi == false && sesVarMi == true) // ilk sesli harf ince ise bu koşul çalışır
+                    {
+                        bool dgrSslrIncMi = true;
+                        for (int i = 0; i < harfler.Length; i++)
+                        {
+                            int sesKontrol2 = Array.IndexOf(inceSesli, harfler[i]); //Diğer sesli harfler ince mi ?
+                            if (sesKontrol2 == -1)
+                            {
+                                dgrSslrIncMi = false;
+                            }
+                        }
+                        if (dgrSslrIncMi == true)
+                        {
+                            panel(true);
+                            lblMesaj.Text = "B.Ü.U 'na uygundur.";
+                        }
+                        else
+                        {
+                            panel(false);
+                            lblMesaj.Text = "B.Ü.U'na uygun değildir.";
+                        }
+                    }
+                    else
+                    { }
+                }
+                else
+                {
+                    aradaBoslukVarmi = false;
+                }
+               
+            }
+            else // Eğer hiçbirşey yazılmazsa bu koşul çalışır.
+            {
+                MessageBox.Show("Lütfen bir kelime giriniz!");
+            }
+
+        }
+        // Kontrol Et butonu aktif olur
+        //textbox a tıklanması halinde texbox temizlenir
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            btnKontrol.Enabled = true;
+            textBox1.Clear();
+        }
+    }
+    //Fatih Yıldızhan && DoXa && 09'
+    //fatih@yildizhanlar.com
+    //www.fatihyildizhan.com
+}
