0ce215505df9ca8461d65a0dca8e9add69af2541
Fatih YILDIZHAN <fatih@doxalabs.co.uk>
2020-03-11 21:48:36 +0300

    add sleeper

---
+++ FyDoxaPcKapatici/FyDoxaPcKapatici/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace FyDoxaPcKapatici
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblKapanmaSaati = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.nu_dakika = new System.Windows.Forms.NumericUpDown();
+            this.lblDkSnrKpn = new System.Windows.Forms.Label();
+            this.btnIptal = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nu_dakika)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblKapanmaSaati);
+            this.groupBox1.Controls.Add(this.lblDurum);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnKapat);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.nu_dakika);
+            this.groupBox1.Controls.Add(this.lblDkSnrKpn);
+            this.groupBox1.Controls.Add(this.btnIptal);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(439, 372);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bilgisayar Kapatıcı";
+            // 
+            // lblKapanmaSaati
+            // 
+            this.lblKapanmaSaati.AutoSize = true;
+            this.lblKapanmaSaati.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKapanmaSaati.Location = new System.Drawing.Point(8, 279);
+            this.lblKapanmaSaati.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblKapanmaSaati.Name = "lblKapanmaSaati";
+            this.lblKapanmaSaati.Size = new System.Drawing.Size(0, 23);
+            this.lblKapanmaSaati.TabIndex = 33;
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(8, 320);
+            this.lblDurum.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(0, 23);
+            this.lblDurum.TabIndex = 32;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(23, 41);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 23);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Kapanma Saati";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(143, 90);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 23);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Dakika Sonra Kapan";
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKapat.Location = new System.Drawing.Point(169, 140);
+            this.btnKapat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(139, 75);
+            this.btnKapat.TabIndex = 26;
+            this.btnKapat.Text = "Kapat";
+            this.btnKapat.UseVisualStyleBackColor = true;
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.CalendarForeColor = System.Drawing.SystemColors.Info;
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.SystemColors.HotTrack;
+            this.dateTimePicker1.CalendarTitleBackColor = System.Drawing.SystemColors.HotTrack;
+            this.dateTimePicker1.CalendarTitleForeColor = System.Drawing.SystemColors.Info;
+            this.dateTimePicker1.CalendarTrailingForeColor = System.Drawing.SystemColors.Info;
+            this.dateTimePicker1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.dateTimePicker1.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)), true);
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(207, 34);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(100, 26);
+            this.dateTimePicker1.TabIndex = 29;
+            // 
+            // nu_dakika
+            // 
+            this.nu_dakika.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.nu_dakika.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.nu_dakika.ForeColor = System.Drawing.SystemColors.Info;
+            this.nu_dakika.Location = new System.Drawing.Point(27, 84);
+            this.nu_dakika.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nu_dakika.Maximum = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.nu_dakika.Name = "nu_dakika";
+            this.nu_dakika.Size = new System.Drawing.Size(71, 26);
+            this.nu_dakika.TabIndex = 27;
+            // 
+            // lblDkSnrKpn
+            // 
+            this.lblDkSnrKpn.AutoSize = true;
+            this.lblDkSnrKpn.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDkSnrKpn.Location = new System.Drawing.Point(8, 234);
+            this.lblDkSnrKpn.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDkSnrKpn.Name = "lblDkSnrKpn";
+            this.lblDkSnrKpn.Size = new System.Drawing.Size(0, 23);
+            this.lblDkSnrKpn.TabIndex = 34;
+            // 
+            // btnIptal
+            // 
+            this.btnIptal.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnIptal.Location = new System.Drawing.Point(27, 140);
+            this.btnIptal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnIptal.Name = "btnIptal";
+            this.btnIptal.Size = new System.Drawing.Size(139, 73);
+            this.btnIptal.TabIndex = 28;
+            this.btnIptal.Text = "İptal";
+            this.btnIptal.UseVisualStyleBackColor = true;
+            this.btnIptal.Click += new System.EventHandler(this.btnIptal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 401);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sleeper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nu_dakika)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblKapanmaSaati;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnKapat;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.NumericUpDown nu_dakika;
+        private System.Windows.Forms.Label lblDkSnrKpn;
+        private System.Windows.Forms.Button btnIptal;
+    }
+}
+
---
+++ FyDoxaPcKapatici/FyDoxaPcKapatici/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FyDoxaPcKapatici
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // sayac girilen dakikayı saniyeye çevirmek için kullanılmıştır
+        //gerisayım ve sayac2 ekranda kalan dakikayı göstermek için kullanılmıştır
+        int sayac = 0;
+        decimal geriSayim = 0;
+        int sayac2 = 0;
+        //timer 1
+        // labelların ekrandaki görüntüleri kaldırılmıştır
+        // girilen dadika kadar süre geçmiş ise pc kapatma işlemi başlar aksi halde ekrana kalan zaman yazılır
+        //kapat butonu
+        //local zaman ve kullanıcının belirlediği zamanlar saklanır
+        // eğer kullanıcı dakika girmiş ise birinci koşul eğer saati datetimepicker dan vermiş ise ikinci koşul aksi halde else çalışır
+        //koşullarda butonların enable özellikleri ve labellerin visible özellikleri bunun yanında timerlar çalıştırılmıştır
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+
+            string now = DateTime.Now.ToShortTimeString();
+            string userTime = dateTimePicker1.Value.ToShortTimeString();
+
+            if (nu_dakika.Value != 0)
+            {
+                lblDurum.Text = "Kapatma İşlemi Başlamıştır !";
+                lblDkSnrKpn.Visible = true;
+                btnKapat.Enabled = false;
+                btnIptal.Enabled = true;
+                decimal dakika = nu_dakika.Value * 60; //kullanıcının girmiş olduğu dadika saniyeye çevrilmiştir.
+                geriSayim = nu_dakika.Value;
+                sayac = Convert.ToInt32(dakika);
+                timer1.Start();
+            }
+            else if (now != userTime && nu_dakika.Value == 0)
+            {
+                lblDurum.Text = "Kapatma İşlemi Başlamıştır !";
+                lblKapanmaSaati.Visible = true;
+                btnKapat.Enabled = false;
+                btnIptal.Enabled = true;
+                timer2.Start();
+            }
+            else
+            {
+                MessageBox.Show("Kapanma Saatini Belirleyiniz!");
+                lblDkSnrKpn.Visible = false;
+                lblDurum.Text = "Kapanma Saatini Belirleyiniz !";
+                lblKapanmaSaati.Visible = false;
+                btnKapat.Enabled = true;
+                btnIptal.Enabled = false;
+            }
+        }
+        // iptal butonu
+        // girilen dadika sıfırlanır aksi halde iki sayaçta aktif hale gelecektir
+        // labelların ekrandaki görüntüleri düzeltilir ve butonların enable olma durumları değiştirilir
+        // uyarı verilir ve timerlar durdurulur
+        private void btnIptal_Click(object sender, EventArgs e)
+        {
+            nu_dakika.Value = 0;
+            lblDkSnrKpn.Visible = false;
+            lblDurum.Text = "Kapanma Saatini Belirleyiniz !";
+            lblKapanmaSaati.Visible = false;
+            btnKapat.Enabled = true;
+            btnIptal.Enabled = false;
+            System.Diagnostics.Process.Start("shutdown", "-a");
+            MessageBox.Show("Kapatma işlemi iptal edilmiştir!");
+            timer1.Stop();
+            timer2.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblDkSnrKpn.Visible = true;
+            lblKapanmaSaati.Visible = false;
+            sayac--;
+            sayac2++;
+            if (sayac == 0)
+            {
+                timer1.Stop();
+                System.Diagnostics.Process.Start("shutdown", "-s -t 50");
+            }
+            else
+            {
+                lblDkSnrKpn.Text = "Bilgisayarınız " + geriSayim + " dakika sonra kapanacaktır!";
+                if (sayac2 == 60)
+                {
+                    geriSayim--;
+                    lblDkSnrKpn.Text = "Bilgisayarınız " + geriSayim + " dakika sonra kapanacaktır!";
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            lblKapanmaSaati.Visible = true;
+            lblDkSnrKpn.Visible = false;
+            string now = DateTime.Now.ToLongTimeString();
+            string userTime = dateTimePicker1.Value.ToLongTimeString();
+            if (now == userTime)
+            {
+                System.Diagnostics.Process.Start("shutdown", "-s -t 50");
+                timer2.Stop();
+            }
+            else
+            {
+                lblKapanmaSaati.Text = "Bilgisayarınız saat " + userTime + " olunca kapanacaktır!";
+            }
+        }
+        // Programın loading olması
+        //labelların visible butonların enable olma durumları ayarlanmıştır.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblDkSnrKpn.Visible = false;
+            lblDurum.Text = "Kapanma Saatini Belirleyiniz !";
+            lblKapanmaSaati.Visible = false;
+            btnIptal.Enabled = false;
+        }
+    }
+    //Fatih Yıldızhan && DoXa && 09'
+    //fatih@yildizhanlar.com
+    //www.fatihyildizhan.com
+}
