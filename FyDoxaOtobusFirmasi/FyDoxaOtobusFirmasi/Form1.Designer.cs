978497ca3730368073f23d5545490ed23012f243
Fatih YILDIZHAN <fatih@doxalabs.co.uk>
2020-03-12 00:23:10 +0300

    a bus company form :bus:

---
+++ FyDoxaOtobusFirmasi/FyDoxaOtobusFirmasi/Form1.Designer.cs
@@ -0,0 +1,414 @@
+﻿namespace FyDoxaOtobusFirmasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.rdbAktarma = new System.Windows.Forms.RadioButton();
+            this.rdbExp = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.cmbNereye = new System.Windows.Forms.ComboBox();
+            this.cmbNerden = new System.Windows.Forms.ComboBox();
+            this.txtSeferNo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmbAramaKalkis = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.chkNerden = new System.Windows.Forms.CheckBox();
+            this.chkTarih = new System.Windows.Forms.CheckBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(632, 206);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnEkle);
+            this.tabPage1.Controls.Add(this.rdbAktarma);
+            this.tabPage1.Controls.Add(this.rdbExp);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.dateTimePicker1);
+            this.tabPage1.Controls.Add(this.cmbNereye);
+            this.tabPage1.Controls.Add(this.cmbNerden);
+            this.tabPage1.Controls.Add(this.txtSeferNo);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(624, 180);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Sefer Ekle";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEkle.Location = new System.Drawing.Point(463, 51);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(155, 66);
+            this.btnEkle.TabIndex = 35;
+            this.btnEkle.Text = "Sefer Ekle >>>";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // rdbAktarma
+            // 
+            this.rdbAktarma.AutoSize = true;
+            this.rdbAktarma.Location = new System.Drawing.Point(143, 97);
+            this.rdbAktarma.Name = "rdbAktarma";
+            this.rdbAktarma.Size = new System.Drawing.Size(68, 17);
+            this.rdbAktarma.TabIndex = 34;
+            this.rdbAktarma.Text = "Aktarmalı";
+            this.rdbAktarma.UseVisualStyleBackColor = true;
+            // 
+            // rdbExp
+            // 
+            this.rdbExp.AutoSize = true;
+            this.rdbExp.Checked = true;
+            this.rdbExp.Location = new System.Drawing.Point(74, 97);
+            this.rdbExp.Name = "rdbExp";
+            this.rdbExp.Size = new System.Drawing.Size(62, 17);
+            this.rdbExp.TabIndex = 33;
+            this.rdbExp.TabStop = true;
+            this.rdbExp.Text = "Express";
+            this.rdbExp.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "Sefer Tip :";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(90, 54);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(121, 20);
+            this.dateTimePicker1.TabIndex = 31;
+            this.dateTimePicker1.Value = new System.DateTime(2008, 10, 27, 0, 0, 0, 0);
+            // 
+            // cmbNereye
+            // 
+            this.cmbNereye.FormattingEnabled = true;
+            this.cmbNereye.Items.AddRange(new object[] {
+            "Istanbul",
+            "Ankara",
+            "Izmir",
+            "Bursa",
+            "Muğla",
+            "Adana"});
+            this.cmbNereye.Location = new System.Drawing.Point(301, 96);
+            this.cmbNereye.Name = "cmbNereye";
+            this.cmbNereye.Size = new System.Drawing.Size(121, 21);
+            this.cmbNereye.TabIndex = 30;
+            // 
+            // cmbNerden
+            // 
+            this.cmbNerden.FormattingEnabled = true;
+            this.cmbNerden.Items.AddRange(new object[] {
+            "Istanbul",
+            "Ankara",
+            "Izmir",
+            "Bursa",
+            "Muğla",
+            "Adana"});
+            this.cmbNerden.Location = new System.Drawing.Point(301, 51);
+            this.cmbNerden.Name = "cmbNerden";
+            this.cmbNerden.Size = new System.Drawing.Size(121, 21);
+            this.cmbNerden.TabIndex = 29;
+            // 
+            // txtSeferNo
+            // 
+            this.txtSeferNo.Location = new System.Drawing.Point(77, 15);
+            this.txtSeferNo.Name = "txtSeferNo";
+            this.txtSeferNo.Size = new System.Drawing.Size(100, 20);
+            this.txtSeferNo.TabIndex = 28;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Sefer Tarihi :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(233, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Varış Yeri :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(233, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Kalkış Yeri :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Sefer No :";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(624, 180);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Sefer Ara";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmbAramaKalkis);
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Controls.Add(this.btnAra);
+            this.groupBox2.Controls.Add(this.chkNerden);
+            this.groupBox2.Controls.Add(this.chkTarih);
+            this.groupBox2.Controls.Add(this.dateTimePicker2);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Location = new System.Drawing.Point(6, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(612, 169);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sefer Arama";
+            // 
+            // cmbAramaKalkis
+            // 
+            this.cmbAramaKalkis.FormattingEnabled = true;
+            this.cmbAramaKalkis.Items.AddRange(new object[] {
+            "Istanbul",
+            "Ankara",
+            "Izmir",
+            "Bursa",
+            "Muğla",
+            "Adana"});
+            this.cmbAramaKalkis.Location = new System.Drawing.Point(86, 47);
+            this.cmbAramaKalkis.Name = "cmbAramaKalkis";
+            this.cmbAramaKalkis.Size = new System.Drawing.Size(121, 21);
+            this.cmbAramaKalkis.TabIndex = 8;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(230, 17);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(376, 143);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Sefer No";
+            this.columnHeader1.Width = 79;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Kalkış Yeri";
+            this.columnHeader2.Width = 85;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Varış Yeri";
+            this.columnHeader3.Width = 75;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Tarih";
+            this.columnHeader4.Width = 67;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Sefer Tip";
+            this.columnHeader5.Width = 63;
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(7, 137);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(216, 23);
+            this.btnAra.TabIndex = 6;
+            this.btnAra.Text = "Sefer Ara >>";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click_1);
+            // 
+            // chkNerden
+            // 
+            this.chkNerden.AutoSize = true;
+            this.chkNerden.Location = new System.Drawing.Point(10, 110);
+            this.chkNerden.Name = "chkNerden";
+            this.chkNerden.Size = new System.Drawing.Size(75, 17);
+            this.chkNerden.TabIndex = 5;
+            this.chkNerden.Text = "Kalkış Yeri";
+            this.chkNerden.UseVisualStyleBackColor = true;
+            // 
+            // chkTarih
+            // 
+            this.chkTarih.AutoSize = true;
+            this.chkTarih.Location = new System.Drawing.Point(10, 80);
+            this.chkTarih.Name = "chkTarih";
+            this.chkTarih.Size = new System.Drawing.Size(50, 17);
+            this.chkTarih.TabIndex = 4;
+            this.chkTarih.Text = "Tarih";
+            this.chkTarih.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(86, 17);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(137, 20);
+            this.dateTimePicker2.TabIndex = 2;
+            this.dateTimePicker2.Value = new System.DateTime(2008, 10, 28, 0, 0, 0, 0);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Kalkış Yeri:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(7, 25);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(34, 13);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Tarih:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 227);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.RadioButton rdbAktarma;
+        private System.Windows.Forms.RadioButton rdbExp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox cmbNereye;
+        private System.Windows.Forms.ComboBox cmbNerden;
+        private System.Windows.Forms.TextBox txtSeferNo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cmbAramaKalkis;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.CheckBox chkNerden;
+        private System.Windows.Forms.CheckBox chkTarih;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ FyDoxaOtobusFirmasi/FyDoxaOtobusFirmasi/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FyDoxaOtobusFirmasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] seferler = new string[0];
+        int sayac = 0;
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (txtSeferNo.Text != "" && cmbNerden.Text != "" && cmbNereye.Text != "")
+            {
+                bool kontrol = true;
+                for (int i = 0; i < seferler.Length; i++)
+                {
+                    string[] dizi = seferler[i].Split(',');
+                    if (dizi[0] == txtSeferNo.Text)
+                    {
+                        kontrol = false;
+                    }
+                }
+
+                if (kontrol == true)
+                {
+                    Array.Resize(ref seferler, sayac + 1);
+
+                    string sefertipi = "";
+                    if (rdbAktarma.Checked == true)
+                    {
+                        sefertipi = "Aktarmalı";
+                    }
+                    else
+                    {
+                        sefertipi = "Ekspres";
+                    }
+
+
+                    string seferbilgisi = txtSeferNo.Text + "," + cmbNerden.SelectedItem.ToString() + "," + cmbNereye.SelectedItem.ToString() + "," + dateTimePicker1.Value.ToShortDateString() + "," + sefertipi;
+
+                    seferler[sayac] = seferbilgisi;
+                    sayac++;
+                }
+                else
+                {
+                    MessageBox.Show("seferler No Mevcuttur!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen gerekli alanları doldurunuz..");
+            }
+
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            if (chkNerden.Checked == true && chkTarih.Checked)
+            {
+
+                foreach (string seferdetayi in seferler)
+                {
+                    string[] seferbilgisi2 = seferdetayi.Split(',');
+                    if (seferbilgisi2[1] == cmbAramaKalkis.SelectedItem.ToString() && seferbilgisi2[3] == dateTimePicker2.Value.ToShortDateString())
+                    {
+                        ListViewItem li = new ListViewItem();
+                        li.Text = seferbilgisi2[0].ToString();
+                        li.SubItems.Add(seferbilgisi2[1].ToString());
+                        li.SubItems.Add(seferbilgisi2[2].ToString());
+                        li.SubItems.Add(seferbilgisi2[3].ToString());
+                        li.SubItems.Add(seferbilgisi2[4].ToString());
+                        listView1.Items.Add(li);
+                    }
+                }
+
+            }
+            else if (chkNerden.Checked)
+            {
+                foreach (string item in seferler)
+                {
+                    string[] seferbilgisi3 = item.Split(',');
+                    if (seferbilgisi3[1] == cmbAramaKalkis.SelectedItem.ToString())
+                    {
+                        ListViewItem li = new ListViewItem();
+                        li.Text = seferbilgisi3[0].ToString();
+                        li.SubItems.Add(seferbilgisi3[1].ToString());
+                        li.SubItems.Add(seferbilgisi3[2].ToString());
+                        li.SubItems.Add(seferbilgisi3[3].ToString());
+                        li.SubItems.Add(seferbilgisi3[4].ToString());
+                        listView1.Items.Add(li);
+                    }
+
+                }
+            }
+            else if (chkTarih.Checked)
+            {
+                foreach (string item in seferler)
+                {
+                    string[] seferbilgisi4 = item.Split(',');
+                    if (seferbilgisi4[3] == dateTimePicker2.Value.ToShortDateString())
+                    {
+                        ListViewItem li = new ListViewItem();
+                        li.Text = seferbilgisi4[0].ToString();
+                        li.SubItems.Add(seferbilgisi4[1].ToString());
+                        li.SubItems.Add(seferbilgisi4[2].ToString());
+                        li.SubItems.Add(seferbilgisi4[3].ToString());
+                        li.SubItems.Add(seferbilgisi4[4].ToString());
+                        listView1.Items.Add(li);
+                    }
+                }
+
+
+            }
+        }
+
+        private void btnAra_Click_1(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            if (!chkNerden.Checked && !chkTarih.Checked)
+            {
+                MessageBox.Show("Lütfen Arama Kriteri Seçiniz!");
+            }
+            else
+            {
+                if (chkNerden.Checked && chkTarih.Checked)
+                {
+
+                    foreach (string seferdetayi in seferler)
+                    {
+                        string[] seferbilgisi2 = seferdetayi.Split(',');
+                        if (seferbilgisi2[1] == cmbAramaKalkis.SelectedItem.ToString() && seferbilgisi2[3] == dateTimePicker2.Value.ToShortDateString())
+                        {
+                            ListViewItem li = new ListViewItem();
+                            li.Text = seferbilgisi2[0].ToString();
+                            li.SubItems.Add(seferbilgisi2[1].ToString());
+                            li.SubItems.Add(seferbilgisi2[2].ToString());
+                            li.SubItems.Add(seferbilgisi2[3].ToString());
+                            li.SubItems.Add(seferbilgisi2[4].ToString());
+                            listView1.Items.Add(li);
+                        }
+                    }
+
+                }
+                else if (chkNerden.Checked)
+                {
+                    foreach (string item in seferler)
+                    {
+                        string[] seferbilgisi3 = item.Split(',');
+                        if (seferbilgisi3[1] == cmbAramaKalkis.SelectedItem.ToString())
+                        {
+                            ListViewItem li = new ListViewItem();
+                            li.Text = seferbilgisi3[0].ToString();
+                            li.SubItems.Add(seferbilgisi3[1].ToString());
+                            li.SubItems.Add(seferbilgisi3[2].ToString());
+                            li.SubItems.Add(seferbilgisi3[3].ToString());
+                            li.SubItems.Add(seferbilgisi3[4].ToString());
+                            listView1.Items.Add(li);
+                        }
+
+                    }
+                }
+                else if (chkTarih.Checked)
+                {
+                    foreach (string item in seferler)
+                    {
+                        string[] seferbilgisi4 = item.Split(',');
+                        if (seferbilgisi4[3] == dateTimePicker2.Value.ToShortDateString())
+                        {
+                            ListViewItem li = new ListViewItem();
+                            li.Text = seferbilgisi4[0].ToString();
+                            li.SubItems.Add(seferbilgisi4[1].ToString());
+                            li.SubItems.Add(seferbilgisi4[2].ToString());
+                            li.SubItems.Add(seferbilgisi4[3].ToString());
+                            li.SubItems.Add(seferbilgisi4[4].ToString());
+                            listView1.Items.Add(li);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //Fatih Yıldızhan && DoXa && 09'
+    //fatih@yildizhanlar.com
+    //www.fatihyildizhan.com
+}
