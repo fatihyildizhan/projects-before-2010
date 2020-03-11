cffe31fd5f78760ad670f983f013a835bb203f29
Fatih YILDIZHAN <fatih@doxalabs.co.uk>
2020-03-11 21:52:57 +0300

    add slot game

---
+++ FyDoxaSlotOyunu/FyDoxaSlotOyunu/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace FyDoxaSlotOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnJetonAl = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblPara = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblJetonOyuncu = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblKasaJeton = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.lblOran = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOyna = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(6, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 22);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Paranız :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 421);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "1 Jeton = 5 YTL";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.DarkKhaki;
+            this.numericUpDown1.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numericUpDown1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.numericUpDown1.Location = new System.Drawing.Point(102, 325);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(47, 45);
+            this.numericUpDown1.TabIndex = 12;
+            // 
+            // btnJetonAl
+            // 
+            this.btnJetonAl.BackColor = System.Drawing.Color.DarkKhaki;
+            this.btnJetonAl.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.btnJetonAl.Location = new System.Drawing.Point(16, 325);
+            this.btnJetonAl.Name = "btnJetonAl";
+            this.btnJetonAl.Size = new System.Drawing.Size(79, 44);
+            this.btnJetonAl.TabIndex = 11;
+            this.btnJetonAl.Text = "Jeton Al";
+            this.btnJetonAl.UseVisualStyleBackColor = false;
+            this.btnJetonAl.Click += new System.EventHandler(this.btnJetonAl_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox4.Location = new System.Drawing.Point(278, 19);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(150, 200);
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.WaitOnLoad = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Peru;
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.btnJetonAl);
+            this.groupBox1.Controls.Add(this.pictureBox4);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.btnOyna);
+            this.groupBox1.ForeColor = System.Drawing.Color.Maroon;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(599, 440);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Slot Machine";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox3.Location = new System.Drawing.Point(434, 19);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(150, 200);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.WaitOnLoad = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox2.Location = new System.Drawing.Point(278, 225);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(150, 200);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.WaitOnLoad = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(434, 225);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 200);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.DarkKhaki;
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.lblPara);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.lblJetonOyuncu);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.lblKasaJeton);
+            this.groupBox2.Controls.Add(this.lblSonuc);
+            this.groupBox2.Controls.Add(this.lblOran);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.groupBox2.Location = new System.Drawing.Point(16, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(239, 234);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            // 
+            // lblPara
+            // 
+            this.lblPara.AutoSize = true;
+            this.lblPara.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPara.Location = new System.Drawing.Point(148, 147);
+            this.lblPara.Name = "lblPara";
+            this.lblPara.Size = new System.Drawing.Size(0, 22);
+            this.lblPara.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(6, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Jeton Sayınız :";
+            // 
+            // lblJetonOyuncu
+            // 
+            this.lblJetonOyuncu.AutoSize = true;
+            this.lblJetonOyuncu.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblJetonOyuncu.Location = new System.Drawing.Point(148, 106);
+            this.lblJetonOyuncu.Name = "lblJetonOyuncu";
+            this.lblJetonOyuncu.Size = new System.Drawing.Size(0, 22);
+            this.lblJetonOyuncu.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kasadaki Jeton :";
+            // 
+            // lblKasaJeton
+            // 
+            this.lblKasaJeton.AutoSize = true;
+            this.lblKasaJeton.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKasaJeton.Location = new System.Drawing.Point(148, 16);
+            this.lblKasaJeton.Name = "lblKasaJeton";
+            this.lblKasaJeton.Size = new System.Drawing.Size(0, 22);
+            this.lblKasaJeton.TabIndex = 3;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(6, 209);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(0, 22);
+            this.lblSonuc.TabIndex = 5;
+            // 
+            // lblOran
+            // 
+            this.lblOran.AutoSize = true;
+            this.lblOran.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOran.Location = new System.Drawing.Point(148, 57);
+            this.lblOran.Name = "lblOran";
+            this.lblOran.Size = new System.Drawing.Size(0, 22);
+            this.lblOran.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(6, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kazanma Şansı :";
+            // 
+            // btnOyna
+            // 
+            this.btnOyna.BackColor = System.Drawing.Color.DarkKhaki;
+            this.btnOyna.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOyna.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.btnOyna.Location = new System.Drawing.Point(16, 259);
+            this.btnOyna.Name = "btnOyna";
+            this.btnOyna.Size = new System.Drawing.Size(239, 57);
+            this.btnOyna.TabIndex = 0;
+            this.btnOyna.Text = "Oyna";
+            this.btnOyna.UseVisualStyleBackColor = false;
+            this.btnOyna.Click += new System.EventHandler(this.btnOyna_Click);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "gez1.jpg");
+            this.ımageList1.Images.SetKeyName(1, "gez2.jpg");
+            this.ımageList1.Images.SetKeyName(2, "gez3.jpg");
+            this.ımageList1.Images.SetKeyName(3, "gez4.jpg");
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 460);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnJetonAl;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblPara;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblJetonOyuncu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblKasaJeton;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Label lblOran;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOyna;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ FyDoxaSlotOyunu/FyDoxaSlotOyunu/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FyDoxaSlotOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // kasadaki para , jeton , kazanılan para , daha önce para verilmiş mi kontrolü , timerlar için sayaç 
+        // kazanma oranları için int tanımları
+        int kasadakiJeton = 8;
+        int oyuncudakiJeton = 2;
+        int kazanilanPara = 2000;
+        bool odul = false;
+        Random r = new Random();
+        int sayacTimers = 0;
+        int sayacTimer2 = 0;
+        int oran1;
+        int oran2;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOyna_Click(object sender, EventArgs e)
+        {
+            lblSonuc.Text = "";
+            btnOyna.Enabled = false; // buton oynanan el bitene kadar kullanılamaz hale getirilir
+            if (oyuncudakiJeton <= 0) // Eğer Jeton Bitmiş İşe Uyarı verilir
+            {
+                btnOyna.Enabled = false;
+                MessageBox.Show("Jetonunuz bitmiştir!");
+            }
+            else // Yeterli Jeton varsa oyun başlar ekrana ilk değerler yazılır
+            {
+                kasadakiJeton++;
+                oyuncudakiJeton--;
+                lblKasaJeton.Text = kasadakiJeton.ToString();
+                lblJetonOyuncu.Text = oyuncudakiJeton.ToString();
+                lblPara.Text = kazanilanPara.ToString() + " YTL";
+                if (kasadakiJeton < 10) // eğer kasadaki jeton 10 dan az ise bu koşul timer 1 i tetikler
+                {
+                    sayacTimers = 25;
+                    timer1.Interval = 80;
+                    timer1.Start();
+                    lblOran.Text = "%0";
+                }
+                else // eğer kasadaki jeton 10 veya fazla ise
+                {
+                    if (odul == false) // daha önce ödül verilmediyse bu koşul timer 2 yi tetikler
+                    {
+                        sayacTimer2 = 30;
+                        timer2.Interval = 80;
+                        timer2.Start();
+                        lblOran.Text = "%20";
+                        oran1 = r.Next(0, 5);
+                    }
+                    else // daha önce ödül verildiyse bu koşul timer 3 ü tetikler
+                    {
+                        sayacTimers = 35;
+                        timer3.Interval = 80;
+                        timer3.Start();
+                        lblOran.Text = "%5";
+                        oran2 = r.Next(0, 20);
+                    }
+                }
+                
+            }
+        }
+        // Jeton Al Butonu
+        // Eğer oyuncu para kazandıysa bu parayı jetona çevirip 
+        // oynamaya devam edebilir...
+        private void btnJetonAl_Click(object sender, EventArgs e)
+        {
+            if (kazanilanPara >= numericUpDown1.Value * 5) // eğer kazanılan para yeterli ise işlem gerçekleşir
+            {
+                btnOyna.Enabled = true;
+                oyuncudakiJeton += Convert.ToInt32(numericUpDown1.Value);
+                lblJetonOyuncu.Text = oyuncudakiJeton.ToString();
+                kazanilanPara -= (Convert.ToInt32(numericUpDown1.Value)) * 5;
+                lblPara.Text = kazanilanPara.ToString(); // kalan para ekranda gösterilir
+                lblSonuc.Text = numericUpDown1.Value.ToString() + " Tane Jeton Aldınız!"; // işlem bilgisi ekranda gösterilir
+                numericUpDown1.Value = 0;
+            }
+            else // oyuncunun parası yetersiz ise uyarı verilir
+            {
+                lblSonuc.Text = "Paranız Yetersiz !";
+            }
+        }
+        // timer 1
+        // timer 1 kasadaki jeton sayısı 10 dan az olduğunda çalışacağı için
+        // hiçbir zaman resimler aynı gelmeyecek ve  oyuncu kaybedecektir
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int rsmNo1 = r.Next(0, ımageList1.Images.Count);
+            int rsmNo2 = r.Next(0, ımageList1.Images.Count);
+            int rsmNo3 = r.Next(0, ımageList1.Images.Count);
+            int rsmNo4 = r.Next(0, ımageList1.Images.Count);
+            pictureBox1.Image = ımageList1.Images[rsmNo1];
+            pictureBox2.Image = ımageList1.Images[rsmNo2];
+            pictureBox3.Image = ımageList1.Images[rsmNo3];
+            pictureBox4.Image = ımageList1.Images[rsmNo4];
+            if (sayacTimers == 0)
+            {
+                timer1.Stop();
+                pictureBox1.Image = ımageList1.Images[0];
+                pictureBox2.Image = ımageList1.Images[1];
+                pictureBox3.Image = ımageList1.Images[2];
+                pictureBox4.Image = ımageList1.Images[3];
+                lblSonuc.Text = "Kaybettiniz!";
+                btnOyna.Enabled = true;
+            }
+            // sayac 0 olduğunca timer ın durması için gereken koşul ve
+            // resimlerin yavaşlayarak durması için intervalin azaltılması
+            else
+            {
+                sayacTimers--;
+                timer1.Interval += 15;
+            }
+        }
+        // timer 2
+        // timer 2 kasadaki jeton 10 ve fazla ise VE daha önce ödül verilmediyse çalışır
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int rsmNo1 = r.Next(0, ımageList1.Images.Count);
+            int rsmNo2 = r.Next(0, ımageList1.Images.Count);
+            int rsmNo3 = r.Next(0, ımageList1.Images.Count);
+            int rsmNo4 = r.Next(0, ımageList1.Images.Count);
+            pictureBox1.Image = ımageList1.Images[rsmNo1];
+            pictureBox2.Image = ımageList1.Images[rsmNo2];
+            pictureBox3.Image = ımageList1.Images[rsmNo3];
+            pictureBox4.Image = ımageList1.Images[rsmNo4];
+            if (sayacTimer2 == 0)
+            {
+                if (oran1 == 0) // butona her tıklandığında yeni ve rastgele üretilen sayı 0 ise oyuncu kazanır -- oran %20 dir 
+                {
+                    timer2.Stop();
+                    Random r2 = new Random();
+                    int resimno = r2.Next(0, 4);
+                    pictureBox1.Image = ımageList1.Images[resimno];
+                    pictureBox2.Image = ımageList1.Images[resimno];
+                    pictureBox3.Image = ımageList1.Images[resimno];
+                    pictureBox4.Image = ımageList1.Images[resimno];
+                    lblSonuc.Text = "Tebrikler Kazandınız!";
+                    lblOran.Text = "%5";
+                    kazanilanPara += 5;
+                    lblPara.Text = kazanilanPara.ToString() + " YTL"; // kazanılan paraya 5 YTL ekler
+                    odul = true; // ödül verildiği için bool u true yapar bu da oranı %5 e düşürür
+                    btnOyna.Enabled = true; // buton aktif hale getirilir ki vatandaş oynasın :D
+                }
+                else // eğer üretilen sayı 0 değil ise oyuncu kaybeder :(
+                {
+                    timer2.Stop();
+                    pictureBox1.Image = ımageList1.Images[0];
+                    pictureBox2.Image = ımageList1.Images[0];
+                    pictureBox3.Image = ımageList1.Images[0];
+                    pictureBox4.Image = ımageList1.Images[3];
+                    lblSonuc.Text = "Kaybettiniz!";
+                    btnOyna.Enabled = true;
+                }
+            }
+            // sayac 0 olduğunca timer ın durması için gereken koşul ve
+            // resimlerin yavaşlayarak durması için intervalin azaltılması
+            else
+            {
+                sayacTimer2--;
+                timer2.Interval += 15;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            int rsmNo1 = r.Next(0, ımageList1.Images.Count);
+            int rsmNo2 = r.Next(0, ımageList1.Images.Count);
+            int rsmNo3 = r.Next(0, ımageList1.Images.Count);
+            int rsmNo4 = r.Next(0, ımageList1.Images.Count);
+            pictureBox1.Image = ımageList1.Images[rsmNo1];
+            pictureBox2.Image = ımageList1.Images[rsmNo2];
+            pictureBox3.Image = ımageList1.Images[rsmNo3];
+            pictureBox4.Image = ımageList1.Images[rsmNo4];
+            if (sayacTimers == 0)
+            {
+                timer3.Stop();
+                if (oran2 == 18)
+                {
+                    Random r3 = new Random();
+                    int resimno = r3.Next(0,4);
+                    pictureBox1.Image = ımageList1.Images[resimno];
+                    pictureBox2.Image = ımageList1.Images[resimno];
+                    pictureBox3.Image = ımageList1.Images[resimno];
+                    pictureBox4.Image = ımageList1.Images[resimno];
+                    lblSonuc.Text = "Tebrikler Kazandınız!";
+                    kazanilanPara += 5;
+                    lblPara.Text = kazanilanPara.ToString() + " YTL";
+                    odul = true;
+                    btnOyna.Enabled = true;
+                }
+                else
+                {
+                    timer3.Stop();
+                    Random r2 = new Random();
+                    int resimno1 = r2.Next(0,2);
+                    int resimno2 = r2.Next(2,4);
+                    pictureBox1.Image = ımageList1.Images[resimno1];
+                    pictureBox2.Image = ımageList1.Images[resimno2];
+                    pictureBox3.Image = ımageList1.Images[resimno1];
+                    pictureBox4.Image = ımageList1.Images[resimno2];
+                    lblSonuc.Text = "Kaybettiniz!";
+                    btnOyna.Enabled = true;
+                }
+            }
+            else
+            {
+                sayacTimers--;
+                timer3.Interval += 15;
+            }
+        }
+    }
+    //Fatih Yıldızhan && DoXa && 09'
+    //fatih@yildizhanlar.com
+    //www.fatihyildizhan.com
+}
